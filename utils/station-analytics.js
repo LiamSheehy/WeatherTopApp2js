1db34b626c2f9d3184ead1a0de629118ca06f962
Glitch (weathertopapp2js) <none>
2023-08-23 23:58:31 +0000

    Updated to utils

---
+++ utils/station-analytics.js
@@ -0,0 +1,14 @@
+export const stationAnalytics = {
+  getShortestReading(station) {
+    let shortestReading = null;
+    if (station.readings.length > 0) {
+      shortestReading = station.readings[0];
+      for (let i = 1; i < station.readings.length; i++) {
+        if (station.readings[i].duration < shortestReading.duration) {
+          shortestReading = station.readings[i];
+        }
+      }
+    }
+    return shortestReading;
+  },
+};
